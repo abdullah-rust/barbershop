889f4f00cdec96d24c0dc17005887c13ee220872
Abdullah Riaz <ariaz7556@gmail.com>
2025-11-19 22:38:06 +0500

    update

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
 // 👈 Isko import karein
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     {/* 👈 App ko iske andar wrap karein */}
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
